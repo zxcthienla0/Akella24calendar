a82347a517bc98bf32268d2bfdff693599a0ca41
zxcThienla0 <semasema6865@gmail.com>
2026-02-05 00:45:14 +0300

    Updates

--- src/models/IUser.ts
+++
@@ -1,5 +0,0 @@
-export interface IUser {
-    id?: number;
-    email: string;
-    password: string;
-}
--- src/models/response/AuthResponse.ts
+++
@@ -1,7 +0,0 @@
-import type {IUser} from "../IUser.ts";
-
-export interface AuthResponse {
-    accessToken: string;
-    refreshToken: string;
-    user: IUser;
-}
